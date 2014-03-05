94b777e0592c3facc732b2cefd16abb4a045a006
Dat Le Tien <datlt_52@vnu.edu.vn>
2014-03-05 18:21:17 +0100

    test

--- FreePastryClient/src/com/dsna/desktop/client/ui/Test.java
+++ FreePastryClient/src/com/dsna/desktop/client/ui/Test.java
@@ -1,5 +1,16 @@
 package com.dsna.desktop.client.ui;
 
+import java.awt.Dimension;
+
+import javax.swing.JFrame;
+
 public class Test {
+	
+    public static void main(String[] args) {
+        JFrame appFrame = new ClientFrame();
+        appFrame.setSize(new Dimension(800, 600));
+        appFrame.setVisible(true);
+        appFrame.show();
+    }
 
 }
