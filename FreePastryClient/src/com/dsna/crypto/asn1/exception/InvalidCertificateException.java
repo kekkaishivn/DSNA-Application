93e66f9eed2d5f7e9ee0957a6a75a3f3c5ab971e
Tien Dat Le <datlt_52@vnu.edu.vn>
2014-05-13 12:51:26 +0200

    Added exception class for ASN1

---
+++ FreePastryClient/src/com/dsna/crypto/asn1/exception/InvalidCertificateException.java
@@ -0,0 +1,9 @@
+package com.dsna.crypto.asn1.exception;
+
+public class InvalidCertificateException extends Exception {
+	
+	public InvalidCertificateException(String s)	{
+		super(s);
+	}
+
+}
---
+++ FreePastryClient/src/com/dsna/crypto/asn1/exception/UnsupportedFormatException.java
@@ -0,0 +1,9 @@
+package com.dsna.crypto.asn1.exception;
+
+public class UnsupportedFormatException extends Exception {
+
+	public UnsupportedFormatException(String description)	{
+		super(description);
+	}
+	
+}
